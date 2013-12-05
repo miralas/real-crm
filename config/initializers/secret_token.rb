be3a90f74db65968e9c06a463e770ae51bacf287
miralas <miralas777@yandex.ru>
2013-12-05 21:01:55 +0400

    Create app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RealCrm::Application.config.secret_key_base = '9774c555977fab07e7155636732c3adedfc0ecdc6e6ef59b6c3f04e03d5336f7c336c5056c60f21b4aba8bf0d505fa494fb0c1f314e98f7d2be5d96da8d9b9cc'
