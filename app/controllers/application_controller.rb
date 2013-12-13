63277e8919562aef935874ded3db766b1720ab2e
miralas <miralas777@yandex.ru>
2013-12-13 08:08:57 +0400

    Other files

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,28 @@
 class ApplicationController < ActionController::Base
+  include ApplicationHelper
+  before_action :authorize
+  
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  
+  protected
+  
+  def authorize
+    if current_user
+      unless current_user.can?(params[:controller], params[:action])
+        redirect_to home_url, notice: 'You no have permissions to do this.'
+      end
+    else
+      redirect_to login_path, notice: "Please log in"
+    end
+  end
+  
+  def set_public_proxy_refresh
+    expires_now if stale? current_user.to_s
+    expires_in 5.minutes, public: true
+  end
+  
+  
+  
 end
--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,2 +1,7 @@
 class MainController < ApplicationController
+  before_action :authorize
+  
+  def index
+    fresh_when [File.mtime('app/views/main/index.html.erb'), current_user], public: true
+  end
 end
